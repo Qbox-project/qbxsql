c623a02055481d19c15adcb12476818b8afbd055
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2026-07-22 18:52:19 +0200

    fix: expose resource aliases to clients

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,6 @@ description 'Database adapter, compatibility layer, and schema manager for FiveM
 version '0.1.0'
 license 'MIT'
 
-server_only 'yes'
 node_version '22'
 
 server_script 'dist/index.js'
@@ -19,12 +18,3 @@ files {
 provide 'oxmysql'
 provide 'mysql-async'
 provide 'ghmattimysql'
-
-convar_category 'qbxsql' {
-    'Configuration',
-    {
-        { 'Connection string', 'mysql_connection_string', 'CV_STRING', 'mysql://root@127.0.0.1/qbxsql' },
-        { 'Slow query warning (ms)', 'qbxsql_slow_query_warning', 'CV_INT', '200' },
-        { 'Debug logging', 'qbxsql_debug', 'CV_BOOL', 'false' }
-    }
-}
